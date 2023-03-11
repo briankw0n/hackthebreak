6a212f6e1d1d1f985831d0a070fd0a2251466ed9
Ravdeep Aulakh <vravdeep@gmail.com>
2023-03-11 11:23:14 -0800

    Menu with pdf upload

--- Menu.py
+++ Menu.py
@@ -1,29 +1,61 @@
 import tkinter as tk
+from tkinter import filedialog
 
 # Create the main window
 window = tk.Tk()
 window.title("Location Menu")
-window.geometry("1000x500")
+window.geometry("300x200")
+window.configure(bg="black")
 
-# Define a function to get the user's location
-def get_location():
-    location = entry.get()
-    print("User location:", location)
+# Define a function to get the user's location and resume
+def get_location_and_resume():
+    location = location_entry.get()
+    resume_path = resume_path_label.cget("text")
+
+    # Save the data or do other processing here
+    # ...
+
+    # Clear the input fields
+    location_entry.delete(0, tk.END)
+
+    # Show a message
+    message_label.config(text="Information submitted successfully", fg="green")
 
 # Configure the font and colors
 font = ("Helvetica", 14)
-label_color = "#444444"
-button_color = "#3366CC"
+label_color = "white"
+button_color = "#800080"
+
+# Create a label for the title text
+title_label = tk.Label(window, text="Hack the Job", font=("Helvetica", 24), fg="white", bg="black", pady=10)
+title_label.pack()
 
 # Create a label and entry field for the user's location
-label = tk.Label(window, text="Enter your location:", font=font, fg=label_color)
-label.pack(pady=10)
-entry = tk.Entry(window, font=font)
-entry.pack(padx=10, pady=10)
+location_label = tk.Label(window, text="Enter your location:", font=font, fg=label_color, bg="black")
+location_label.pack(pady=10)
+location_entry = tk.Entry(window, font=font)
+location_entry.pack(padx=10, pady=5)
 
-# Create a button to submit the user's location
-button = tk.Button(window, text="Submit", font=font, bg=button_color, fg="white", command=get_location)
-button.pack(pady=10)
+# Create a label and button for the user's resume
+resume_label = tk.Label(window, text="Upload your resume (PDF only):", font=font, fg=label_color, bg="black")
+resume_label.pack(pady=10)
+resume_path_label = tk.Label(window, font=font, fg=label_color, bg="black")
+resume_path_label.pack(padx=10, pady=5)
+
+# Create a button to upload the user's resume
+def browse_resume():
+    resume_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+    resume_path_label.config(text=resume_path)
+resume_button = tk.Button(window, text="Browse", font=font, bg=button_color, fg="white", command=browse_resume)
+resume_button.pack(pady=5)
+
+# Create a button to submit the user's location and resume
+submit_button = tk.Button(window, text="Submit", font=font, bg=button_color, fg="white", command=get_location_and_resume)
+submit_button.pack(pady=10)
+
+# Create a label for messages
+message_label = tk.Label(window, font=font, fg="red", bg="black")
+message_label.pack()
 
 # Run the main event loop
 window.mainloop()

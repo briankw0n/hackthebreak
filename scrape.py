2aa881e64063e50cd8e7b3675e136c6c2f20c6f5
Brian Kwon <bkwon6@my.bcit.ca>
2023-03-11 22:01:53 -0800

    scrapes title, company, and link from website

--- scrape.py
+++ scrape.py
@@ -5,27 +5,14 @@ url = 'https://www.bcjobs.ca/search-jobs?q=software+developer&location='
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-job_listings = soup.find_all('div', id='joblist-body-content')
+job_listings = soup.find_all('a', class_='list-item-wrapper')
 
 for job in job_listings:
     title = job.find('div', class_='list-item-title').text.strip()
     company = job.find('div', class_='list-divider').text.strip()
-    link = job.find('a', href=True)['href']
+    link = job.get('href')
 
     print(f"Title: {title}")
     print(f"Company: {company}")
     print(f"Link: {link}")
     print()
-    
-
-##div_elements = soup.find_all('div', class_='u_p-none')
-##
-##for div in div_elements:
-##    title = div.find('div', class_='list-item-title')
-##    company = div.find('div', class_='text-limit')
-##    link = div.find('a', href=True)['href']
-##
-##    print(title)
-##    print(company)
-##    print(link)
-    

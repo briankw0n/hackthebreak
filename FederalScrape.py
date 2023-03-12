7e4e6573f3021336144faacce03ec2ffca555a1c
chino0522 <baekjinho0522@gmail.com>
2023-03-12 01:13:39 -0800

    Federal post in progress

--- FederalScrape.py
+++ FederalScrape.py
@@ -1,22 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-list = []
-url = 'https://www.jobbank.gc.ca/jobsearch/jobsearch?searchstring=software+developer&locationstring=Vancouver%2C+BC'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+def get_jobDescription(url):
 
-job_listings = soup.find_all('article')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-i = 0
-for job in job_listings:
-    list.append(job.find('span', class_="distance").text.strip())
-    list.append(job.find('li', class_="business").text.strip())
-    list.append(job.find('li', class_="date").text.strip())
-    list.append(job.find('span', class_="wb-inv").text.strip())
-    list.append(job.find('span', class_="fa fa-dollar").text.strip())
-    list.append(job.a['href'])
-    i = i + 1
+    job_listings = soup.find_all('article')
 
-print(list)
+    list = []
 
+    for job in job_listings:
+        distance = job.find('span', class_="distance").text.strip()
+        company = job.find('li', class_="business").text.strip()
+        postdate = job.find('li', class_="date").text.strip()
+        temp = job.find('li', class_="location").text.strip().replace("\n", "")
+        temp = temp.replace("\t", "")
+        temp = temp.replace("Location", "")
+        temp = temp.replace(" ", "")
+        location = temp
+        postURL = job.a['href']
+        list.append({
+            'distance': distance,
+            'company': company,
+            'postdate': postdate,
+            'location': location,
+            'postURL': postURL
+        })
+
+    return list

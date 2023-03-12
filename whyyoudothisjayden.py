12c6885a0f2cc6fec9a4d54f5bedc91e658b5a2b
Brian Kwon <bkwon6@my.bcit.ca>
2023-03-11 23:59:18 -0800

    sad

---
+++ whyyoudothisjayden.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_job_description():
+  url = 'https://www.bcjobs.ca/search-jobs?q=software+developer&location='
+  response = requests.get(url)
+  soup = BeautifulSoup(response.content, 'html.parser')
+
+  job_listings = soup.find_all('a', class_='list-item-wrapper')
+
+  jobs = []
+
+  for job in job_listings:
+      title = job.find('div', class_='list-item-title').text.strip()
+      company = job.find('div', class_='list-divider').text.strip()
+      link = job.get('href')
+
+      jobs.append({
+          'title': title,
+          'company': company,
+          'link': link
+      })
+
+  return jobs
